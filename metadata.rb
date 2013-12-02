34602bf966fbd6db647846a4634234ca61db2154
pigram86 <todd@toddpigram.com>
2013-12-02 12:45:28 -0500

    attr added

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'silverlight_x64'
+maintainer       'Todd Pigram'
+maintainer_email 'todd@toddpigram.com'
+license          'Apache 2.0'
+description      'Installs/Configures silverlight_x64'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '1.1.0'
+depends          "windows"
+
